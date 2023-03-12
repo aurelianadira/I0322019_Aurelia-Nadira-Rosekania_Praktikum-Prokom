c613981b37b0c40d9a3db327232dd0bb2e38b274
aurelianadira <aurelianadira19@student.uns.ac.id>
2023-03-12 22:49:11 +0700

    exercise01

---
+++ exercise-01.py
@@ -0,0 +1,18 @@
+#exercise-01
+
+print("="*55)
+print("\n"+"Nama     : Aurelia Nadira Rosekania D")
+print("\n"+"NIM      : I0322019")
+print("="*55)
+print(""" 
+Halo semuanya, perkenalkan nama aku Aurelia Nadira Rosekania bisa dipanggil aurell.
+Aku berasal dari ibu kota Indonesia yakni Jakarta,
+Kota jakarta merupakan kota yang ditinggali oleh beragam suku-suku di indonesia,
+dikarenakan Jakarta sendiri termasuk ke dalam salah satu kota yang memiliki fasilitas yang terhitung sangat lengkap.
+Hal itu tentunya sangat menarik orang untuk tinggal di wilayahnya. Banyak sekali tempat-tempat yang harus
+dikunjungi saat kita sedang berada di jakarta, serta kuliner nya pun tidak kalah lengkapnya, 
+konser-konser juga sering kali mengadakan serta mengutamakan kota Jakarta, yang tentunya membuatku sangat betah untuk
+tinggal disana, terlepas dari kemacetannya, jakarta sungguh merupakan kota yang sekarang saya rindukan
+    Harapanku sebagai seorang mahasiswi teknik industri setelah lulus adalah yakni dapat menerapkan ilmu-ilmu
+     serta memanfaatkannya untuk hal-hal yang dapat memajukan bangsa """)
+print("="*55)
